4130ed75ad8eba016f7240d33e3e7e798a2a2ed4
Erik Feeley <efeeley@vectorform.com>
2016-11-18 14:24:45 -0500

    playing functions, statements, expressions, IIFEs.

--- JavaScriptUnderstandWeirdParts/B4_Hoisting/Starter/app.js
+++ JavaScriptUnderstandWeirdParts/B4_Hoisting/Starter/app.js
@@ -1,7 +1,37 @@
-function greet(firstName, lastName, language) {
-    console.log(firstName);
-    console.log(lastName);
-    console.log(language);
+// function statement
+function greet(name) {
+    console.log('Hello ' + name);
 }
+greet('John');
 
-greet();
+
+// function expression
+// not initialized but created on the fly when the javascript engine gets to this line.
+var greetFunc = function (name) {
+    console.log('Hello ' + name);
+};
+greetFunc('John'); // invoke with variable pointing to that memory location.
+
+var greeting = function(name) {
+    return 'Hello ' + name;
+}('John'); // IIFE invoked immediately after creation
+console.log(greeting); // notice where we pass the arguments.
+
+// console.log(greeting()); // <-- cant do this because greeting is
+                        // a string and not a function since it was 
+                        // immediately invoked...
+
+// wrap in parens to allow this. otherwise it expects a function name.
+// now it assumes this will be a function expression instead of
+// a function statement.
+var firstName = 'John';
+
+(function(name) {
+    var greeting = 'Inside IIFE:  hello';
+    console.log(greeting + ' ' + name);
+})(firstName); // the iffe weeeeeeee wwooooowww uhhh ahhhhhh
+
+// IIFE is apparently a legit ass tool... prob something about dem
+// modules or some shizzzzzz....
+
+// also we can use the invoke () inside the outer parens or outside them.
