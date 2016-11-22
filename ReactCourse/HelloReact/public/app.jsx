f1975b40a19d93da6e87ca4a8f054a368fea9e44
Erik Feeley <efeeley@vectorform.com>
2016-11-22 13:53:59 -0500

    playing with nested components

--- ReactCourse/HelloReact/public/app.jsx
+++ ReactCourse/HelloReact/public/app.jsx
@@ -1,19 +1,50 @@
+var GreeterMessage = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>Some H1</h1>
+                <p>Some paragraph</p>
+            </div>
+        );
+    }
+});
+
+var GreeterForm = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <form>
+                    <input type="text" ref="name"/>
+                    <button>Set Name</button>
+                </form>
+            </div>
+        );
+    }
+});
+
 // create react component
 var Greeter = React.createClass({
+    // get default props. these are different than state. components dont update their own props
     getDefaultProps: function () {
         return {name: 'React', message: 'u forgot message dummy'};
     },
+    // similar to get default props
+    // gets initial state for the component
+    // in this case from the prop
     getInitialState: function () {
-        // similar to get default props
         return {name: this.props.name};
     },
     onButtonClick: function (e) {
+        // prevent default to make sure no browser refresh
+        // we are doing react spa things... so yeah....
         e.preventDefault();
 
         var nameRef = this.refs.name;
         var name = nameRef.value;
-        nameRef.value = ''; // resets input box
+        // resets input box
+        nameRef.value = '';
 
+        // if simple validation validates do the thing.
         if (typeof name === 'string' && name.length > 0) {
             this.setState({name: name});
         }
@@ -29,10 +60,14 @@ var Greeter = React.createClass({
                     {message}
                 </p>
 
+                <GreeterMessage />
+
                 <form onSubmit={this.onButtonClick}>
                     <input type="text" ref="name"/>
                     <button>Set Name</button>
                 </form>
+
+                <GreeterForm />
             </div>
         );
     }
