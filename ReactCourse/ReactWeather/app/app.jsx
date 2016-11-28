6583fdf98a01bef7d68db62f0f76de88701cb5e8
Erik Feeley <efeeley@vectorform.com>
2016-11-28 14:59:36 -0500

    laying out site structure with react routes

--- ReactCourse/ReactWeather/app/app.jsx
+++ ReactCourse/ReactWeather/app/app.jsx
@@ -4,13 +4,15 @@ var ReactDOM = require('react-dom');
 // fancy es6 destructuing syntax for the less typing of the things and stuff.
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
-
-
+var Weather = require('Weather');
+var About = require('About');
+var Examples = require('Examples');
 
 ReactDOM.render(
     <Router history={hashHistory}>
-        <Route path="/" component={Main}>
-
-        </Route>
-    </Router>,
-    document.getElementById('app'));
+    <Route path="/" component={Main}>
+        <Route path="about" component={About}/>
+        <Route path="examples" component={Examples}/>
+        <IndexRoute component={Weather}/>
+    </Route>
+</Router>, document.getElementById('app'));
