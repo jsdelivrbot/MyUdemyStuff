9686e20ab94623c22b465459f527e1339436b3e4
Erik Feeley <efeeley@vectorform.com>
2017-05-17 21:00:38 -0400

    4-38 done

--- ReactRedux/SimpleStarter/src/index.js
+++ ReactRedux/SimpleStarter/src/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './Components/SearchBar';
 import VideoList from './Components/VideoList';
@@ -17,18 +18,24 @@ class App extends Component {
       selectedVideo: null
     };
 
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
     YTSearch({
       key: API_KEY,
-      term: 'surfboards'
+      term: term
     }, (videos) => {
       this.setState({videos: videos, selectedVideo: videos[0]});
     });
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar/>
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
