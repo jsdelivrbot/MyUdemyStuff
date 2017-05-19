9b8868233ba16f886b0b3c67b5436ecb7267ee27
Erik Feeley <efeeley@vectorform.com>
2017-05-19 18:13:46 -0400

    done up to 4-46

--- ReactRedux/ReduxPractice/src/Containers/BookList.js
+++ ReactRedux/ReduxPractice/src/Containers/BookList.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import {selectBook} from '../actions/index';
 
 class BookList extends Component {
     renderList() {
-        return this.props.books.map((book) => {
-            return (
-                <li key={book.title} className="list-group-item">{book.title}</li>
-            );
-        });
+        return this
+            .props
+            .books
+            .map((book) => {
+                return (
+                    <li
+                        key={book.title}
+                        onClick={() => this.props.selectBook(book)}
+                        className="list-group-item">
+                        {book.title}
+                    </li>
+                );
+            });
     }
 
     render() {
@@ -20,12 +31,22 @@ class BookList extends Component {
 }
 
 function mapStateToProps(state) {
-    // whatever returned shows up as props in booklist
-    // takes state what is returned form here shows up as props in the container
-    return {
-        books: state.books
-    };
+    // whatever returned shows up as props in booklist takes state what is returned
+    // form here shows up as props in the container
+    return {books: state.books};
 }
 
-// this is what actually connects react and redux
-export default connect(mapStateToProps)(BookList);
+// anything returned from this function will end up as props on the book list
+// container now we can call this.props.selectBook because of this stuff.
+function mapDispatchToProps(dispatch) {
+    // whenever selectBook is called the result should be passed to all of our
+    // reducers.
+    return bindActionCreators({
+        selectBook: selectBook
+    }, dispatch);
+}
+
+// this is what actually connects react and redux promote booklist from
+// component to container it needs to know about this new dispatch method
+// selectbook make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
