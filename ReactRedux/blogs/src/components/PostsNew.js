ffc306159a9f5e46cfc35760114b7d561d4be7ec
Erik Feeley <efeeley@vectorform.com>
2017-05-24 23:09:08 -0400

    at 145 almost done with blogs

--- ReactRedux/blogs/src/components/PostsNew.js
+++ ReactRedux/blogs/src/components/PostsNew.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   // arg field for usage for the Field comp
@@ -11,21 +14,42 @@ class PostsNew extends Component {
   // onChange={field.input.onCHange}
   // one thing is much better.. prolly
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
 
+  // so first handleSubmit is a thing that comes from redux-form
+  // basically it says hey if redux-form goes through and all is 
+  // well then call the callback function that we make here.
+  // also more annoying bind(this) bs so that we make sure
+  // that this === component
+  onSubmit(values) {
+    // we get access to props.history because this is getting
+    // rendered by the Route component
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
@@ -41,6 +65,8 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
@@ -48,7 +74,8 @@ class PostsNew extends Component {
 
 // values by convention
 // all values entered into form.
-// keyed by Field name
+// keyed by Field name - important names must match.
+// doing a validate function adds field.meta.error
 function validate(values) {
   const errors = {};
 
@@ -58,11 +85,11 @@ function validate(values) {
   }
 
   if (!values.categories) {
-    errors.title = 'Enter some categories!';
+    errors.categories = 'Enter some categories!';
   }
 
   if (!values.content) {
-    errors.title = 'Enter some content!';
+    errors.content = 'Enter some content!';
   }
 
   // if errors is empty the form is fine to submit
@@ -72,8 +99,12 @@ function validate(values) {
 
 // redux form yo.
 // form: literally name of form
+// also passing in validate function
 // cuz maybe you have more than one form on page.
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  // getting kinda nutty in here.
+  connect(null, { createPost })(PostsNew)
+);
---
+++ ReactRedux/blogs/src/components/PostsShow.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions';
+
+class PostsShow extends Component {
+  // lifecycle method... watch that spelling.
+  componentDidMount() {
+    // we get this from react router params is for wildcards in the url.
+    const {id} = this.props.match.params;
+    this
+      .props
+      .fetchPost();
+  }
+
+  render() {
+    return (
+      <div>
+        Posts Show!
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({posts}) {
+  return { posts };
+}
+
+export default connect(null, {fetchPost})(PostsShow);
