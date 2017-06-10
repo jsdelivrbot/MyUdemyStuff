7941dbfc7a678e4b33e40f73b59cc776be86ffe8
ErikFeeley <ehfeeley@gmail.com>
2017-06-10 18:25:20 -0400

    working on me new webpack course

---
+++ WebPackTwo/JsModules/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    // modifies how url loader appends the path when generating its output.
+    // publicpath is used by any loader doing pathy things.
+    publicPath: 'build/'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      },
+      {
+        // order matters here, its actually done
+        // right to left tho
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
+        test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
+};
+
+module.exports = config;
