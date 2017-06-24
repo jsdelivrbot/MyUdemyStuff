d4b8338b7a193138118f3c5d8768f64f6514066a
ErikFeeley <ehfeeley@gmail.com>
2017-06-24 19:05:52 -0400

    getting router in

--- TravelTimers/src/index.tsx
+++ TravelTimers/src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import { ChillComponent } from './components';
 import styles from './style/style.css';
 
-const App: React.SFC<{}> = props => (
-  <div className={styles.awesomeStyle}>
-    aight lets be chill
-    <ChillComponent />
-  </div>
-);
+class App extends Component<{}, {}>  {
+  render() {
+    return (
+      <Router>
+        <Route path="/" component={ChillComponent} />
+      </Router>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
